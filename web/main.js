f14d47d4c5a8c90c3eb35715d4b821e625047825
Rocean <roceanadhikari@gmail.com>
2026-04-18 19:11:42 +0530

    removed the web

--- web/main.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-import React from "react";
-import { createRoot } from "react-dom/client";
-import { OsanwallCanvasApp } from "./screen";
-import "./styles.css";
-const root = document.getElementById("root");
-if (!root)
-    throw new Error("Missing root element");
-createRoot(root).render(_jsx(React.StrictMode, { children: _jsx(OsanwallCanvasApp, {}) }));
